1b802941476747f690722d1b3251e0bd10c07997
muskanqed <muskanqed@gmail.com>
2024-10-25 13:18:26 +0530

    Initial steps creating routes skeleton

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+
+});
+
+app.post("/login", (req, res) => {
+
+});
+
+app.get("/listofCourses", (req, res) => {
+
+});
+
+app.post("/purchaseCourses", (req, res) => {
+
+});
+
+app.get("/purchasedCourses", (req, res) => {
+
+});
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`serving running on port 3000`);
+
+});
